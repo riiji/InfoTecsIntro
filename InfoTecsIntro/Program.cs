18b8dc48f5cb223ddcc245a5ee0e6bbaced806fd
Riijii <riijii333@gmail.com>
2020-08-23 10:09:24 -0700

    Initial

---
+++ InfoTecsIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfoTecsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
