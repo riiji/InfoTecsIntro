c31944e69a1cf49de6d235ab46b5af08ebb7ebbd
Riijii <riijii333@gmail.com>
2020-08-24 21:18:52 +0300

    feat: config

---
+++ InfoTecsIntro/Config/IGetConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace InfoTecsIntro.Config
+{
+    public interface IGetConfig<out T>
+    {
+        public T GetConfig();
+    }
+}
---
+++ InfoTecsIntro/Config/JsonConfig.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace InfoTecsIntro.Config
+{
+    public class JsonConfig<T> : IGetConfig<T>
+    {
+        private readonly string _jsonPath;
+
+        public JsonConfig(string jsonPath)
+        {
+            _jsonPath = jsonPath;
+        }
+
+        public T GetConfig()
+        {
+            return JsonConvert.DeserializeObject<T>(_jsonPath);
+        }
+    }
+}
