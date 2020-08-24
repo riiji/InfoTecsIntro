fbc59f7237109e22b6c2f759de1e5d4076c06380
Riijii <riijii333@gmail.com>
2020-08-24 21:19:11 +0300

    feat: extension tools

---
+++ InfoTecsIntro/ObjectExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InfoTecsIntro
+{
+    public static class ObjectExtension
+    {
+        public static void ThrowIfNull(this object obj, string exceptionMessage)
+        {
+            if (obj == null)
+                throw new ArgumentException(exceptionMessage);
+        }
+    }
+}
